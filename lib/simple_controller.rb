4fecd996dcb0f3effc5f5978a57cb51817c1fa0c
Philipp Neugebauer <ppn@nexgo.de>
2015-01-09 19:46:48 +0100

    code in one file works at least

--- lib/simple_controller.rb
+++ lib/simple_controller.rb
@@ -1,5 +1,72 @@
 # encoding: UTF-8
 module SimpleController
+  module CreateController
+    include SimpleController
+
+    def new
+      setup_instance_variable model_name.new
+    end
+
+    def create
+      setup_instance_variable model_name.new(model_params)
+
+      if model_instance_variable.save
+        redirect_to action: 'index', notice: I18n.t('successful_creation')
+      else
+        render 'new'
+      end
+    end
+  end
+
+  module DestroyController
+    include SimpleController
+    #before_action :set_object, only: [:destroy]
+
+    def destroy
+      set_object
+      model_instance_variable.destroy
+      redirect_to action: 'index', notice: I18n.t('successful_deletion')
+    end
+  end
+
+  module UpdateController
+    include SimpleController
+    #before_action :set_object, only: [:edit, :update]
+
+    def edit
+      set_object
+    end
+
+    def update
+      set_object
+      if model_instance_variable.update(model_params)
+        redirect_to action: 'index', notice: I18n.t('successful_update')
+      else
+        render 'edit'
+      end
+    end
+  end
+
+  module IndexController
+    include SimpleController
+
+    def index
+      setup_instance_variable(model_name.all, model_name.to_s.pluralize.underscore)
+    end
+  end
+
+  module ShowController
+    include SimpleController
+    #include AbstractController::Callbacks
+    #include ActiveSupport::Callbacks
+
+    def show
+      set_object
+    end
+    #before_action :set_object, only: [:show]
+  end
+
+
   def model_name_as_sym
     model_name.to_s.underscore
   end
@@ -17,7 +84,7 @@ module SimpleController
   end
 
   def model_params
-    params.require(model_name_as_sym.to_sym).permit(model_name.attribute_names.collect {|name| name.to_sym})
+    params.require(model_name_as_sym.to_sym).permit(model_name.attribute_names.collect { |name| name.to_sym })
   end
 
   def set_object
