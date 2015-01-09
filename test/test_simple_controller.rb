6e299e882b91db84eeef5a5dd912f65a93c1cb3e
Philipp Neugebauer <ppn@nexgo.de>
2015-01-10 00:04:36 +0100

    renamed test file

--- test/test_simple_controller.rb
+++ test/test_simple_controller.rb
@@ -1,15 +1,14 @@
-require 'action_controller'
 require 'minitest/autorun'
 require 'simple_controller'
 
 class EasyController < SimpleController
 end
 
-class SimpleControllerTest < Minitest::Test
+class TestSimpleController < Minitest::Test
 
-  #def test_model_name
-    #assert_equal('Easy', EasyController.new.model_name)
-  #end
+  def test_model_name
+    assert_equal('Easy', EasyController.new.model_name)
+  end
 
   #def test_modulized_model_name
     #assert_equal('Easy', Admin::EasyController.new.model_name)
