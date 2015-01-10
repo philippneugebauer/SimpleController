6141f9aa16a5840034855ea87f6bcd37bc6d4356
Philipp Neugebauer <ppn@nexgo.de>
2015-01-10 01:17:26 +0100

    test can now be executed #4

--- test/test_simple_controller.rb
+++ test/test_simple_controller.rb
@@ -1,13 +1,21 @@
 require 'minitest/autorun'
+require 'rake/testtask'
+require 'active_support/inflector'
+require 'active_support/concern'
 require 'simple_controller'
 
-class EasyController < SimpleController
+class EasyController
+  extend ActiveSupport::Inflector
+  include SimpleController
+end
+
+class Easy
 end
 
 class TestSimpleController < Minitest::Test
 
   def test_model_name
-    assert_equal('Easy', EasyController.new.model_name)
+    assert_equal(Easy, EasyController.new.model_name)
   end
 
   #def test_modulized_model_name
