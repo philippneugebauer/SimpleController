f01d6db0a8a04bca625be7516afdf34366655904
Philipp Neugebauer <ppn@nexgo.de>
2015-01-10 02:28:31 +0100

    two more tests

--- test/test_simple_controller.rb
+++ test/test_simple_controller.rb
@@ -28,4 +28,16 @@ class TestSimpleController < Minitest::Test
   def test_modulized_model_name
     assert_equal(Easy, Admin::EasyController.new.model_name)
   end
+
+  def test_model_name_as_sym
+    assert_equal("easy", EasyController.new.model_name_as_sym)
+  end
+
+  def test_instance_variable
+    e = EasyController.new
+    assert_nil(e.model_instance_variable)
+    e.setup_instance_variable("HELLO")
+    assert_equal("HELLO", e.model_instance_variable.to_s)
+    assert(String, e.model_instance_variable.class)
+  end
 end
