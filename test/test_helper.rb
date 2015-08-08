da307e94dd9dcdbb13b24bd538cc97803dd0dad4
Philipp Neugebauer <ppn@philippneugebauer.net>
2015-08-08 16:05:27 +0200

    coverage fix for code climate

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,9 +4,9 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
 require "rails/test_help"
+require "codeclimate-test-reporter"
 
 if ENV['COVERAGE']
-  require "codeclimate-test-reporter"
   CodeClimate::TestReporter.start
 end
 
